5fa818960f9317e5546b639bb28b5c8e6fdd65f0
Macasti4 <109395818+Macasti4@users.noreply.github.com>
2023-06-22 18:40:24 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,70 +1,18 @@
 import requests
-import pandas as pd
 import xml.etree.ElementTree as ET
-def transformar(source):
+from xml.etree.ElementTree import fromstring, ElementTree
+import win32com.client
+import time
+import os 
+import pandas as pd
 
-	# Parsear el archivo XML
-	tree = ET.parse(source)
-	root = tree.getroot()
-
-	# Crear una lista para almacenar los datos
-	data = []
-
-	# Recorrer los elementos XML y extraer los datos
-	for child in root.iter('row'):
-	    codigo = child.find('codigo').text
-	    descripcion = child.find('descripcion').text
-	    equivalencia = child.find('equivalencia').text
-	    marca = child.find('marca').text
-	    familia = child.find('familia').text
-	    familia_id = child.find('familia_id').text
-	    stock = child.find('stock').text
-	    image_url = child.find('image_url').text
-	    medida = child.find('medida').text
-	    peso = child.find('peso').text
-	    privacidad = child.find('privacidad').text
-	    precio = child.find('precio').text
-
-	    # Agregar los datos a la lista
-	    data.append({
-	        'codigo': codigo,
-	        'descripcion': descripcion,
-	        'equivalencia': equivalencia,
-	        'marca': marca,
-	        'familia': familia,
-	        'familia_id': familia_id,
-	        'stock': stock,
-	        'image_url': image_url,
-	        'medida': medida,
-	        'peso': peso,
-	        'privacidad': privacidad,
-	        'precio': precio
-	    })
-
-	# Crear un DataFrame de pandas a partir de la lista de datos
-	df = pd.DataFrame(data)
-
-	# Realizar las transformaciones necesarias en el DataFrame
-	df['familia_id'] = df['familia_id'].astype(int)
-	df['stock'] = df['stock'].astype(int)
-	df['equivalencia'] = df['equivalencia'].astype(int)
-	df['precio'] = df['precio'].astype(float)
-
-	# Eliminar columnas no deseadas
-	df = df.drop(columns=['modelo', 'item_grupo_1', 'item_grupo_2', 'item_grupo_3', 'image_url_2', 'image_url_3', 'image_url_4', 'image_url_5', 'caracteristicas', 'privacidad', 'peso', 'itemref_1', 'itemref_2', 'itemref_3', 'itemref_4', 'itemref_5', 'itemref_6', 'itemref_7', 'itemref_8', 'itemref_9', 'itemref_10', 'itemref_11', 'itemref_12', 'itemref_13', 'itemref_14'])
-
-	# Mostrar el DataFrame resultante
-	print(df)
-
-
-
-def realizarSolicitudSOAP():
+def realizarsolicitudsoap():
     url = "https://www.eurocompcr.com/webservice.php?wsdl"
     usuario = 1550
-    contraseña = "15502022"
+    contrasena = "15502022"
     bid = 1
 
-    xmlData = '''<?xml version="1.0" encoding="UTF-8"?>
+    xmldata = '''<?xml version="1.0" encoding="UTF-8"?>
         <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:server">
             <soapenv:Header/>
             <soapenv:Body>
@@ -74,25 +22,200 @@ def realizarSolicitudSOAP():
                     <bid>{}</bid>
                 </urn:wsc_request_bodega_all_items>
             </soapenv:Body>
-        </soapenv:Envelope>'''.format(usuario, contraseña, bid)
+        </soapenv:Envelope>'''.format(usuario, contrasena, bid)
 
     headers = {"Content-Type": "text/xml"}
-    response = requests.post(url, data=xmlData, headers=headers)
+    response = requests.post(url, data=xmldata, headers=headers)
 
-    responseCode = response.status_code
-    responseData = response.text
+    responsecode = response.status_code
+    responsedata = response.text
 
-    if responseCode == 200:
-        #print(responseData)
-        guardar(responseData)
-        transformar("Inventario_EUROCOMP.xml");
+    if responsecode == 200:
+        return responsedata
+
     else:
-        print("Error en la solicitud SOAP. Código de respuesta:", responseCode)
+        print("Error en la solicitud SOAP. Código de respuesta:", responsecode)
 
-def guardar(responseData):
-	with open("Inventario_EUROCOMP.xml", "w") as archivo:
-		archivo.write(responseData)
-		print("Archivos guardados exitosamente")
+def guardar(responsedata, nombre):
+    with open(nombre, "w") as archivo:
+        archivo.write(responsedata)
+        print("Archivo XML guardado exitosamente")
 
-# Llamada a la función principal
-realizarSolicitudSOAP()
+def actualizarDatosExcel():
+        print("Actualizando datos Excel...")
+        xlapp = win32com.client.DispatchEx("Excel.Application")
+        wb = xlapp.Workbooks.open(os.getcwd()+"\\Inventario_Eurocomp.xlsx")
+        wb.RefreshAll()
+        xlapp.CalculateUntilAsyncQueriesDone()
+        xlapp.DisplayAlerts = False
+        wb.Save() 
+        xlapp.Quit() 
+        salir = True
+        print("Datos actualizados")
+
+def exportarExcelACsv():
+    print("Exportando Excel actualizado a CSV")
+    salir = False
+    while salir == False:
+        try:
+            wb = pd.read_excel ("Inventario_Eurocomp.xlsx")
+            #wb.to_csv ("Inventario_Eurocomp.csv",encoding='utf-8-sig', index=False)
+            salir = True
+            csv_string = wb.to_csv(index=False, sep=',')
+            print("Exportacion a CSV exitosa")
+        except PermissionError as e:
+            print("ERROR: Reitentando guardar el CSV")
+            time.sleep(0.25)
+    return csv_string
+
+def tipoDeCambio():
+    # URL de la API
+    url = 'https://tipodecambio.paginasweb.cr/api'
+
+    # Realiza la solicitud GET a la API
+    response = requests.get(url)
+
+    # Verificar el estado de la respuesta
+    if response.status_code == 200:
+        # Obtener el tipo de cambio del JSON de respuesta
+        data = response.json()
+        tipo_cambio = data['venta']
+        print(tipo_cambio)
+        return tipo_cambio
+    else:
+        print('Error al obtener el tipo de cambio.')
+
+def transformar(responsedata):
+    #se convierte el xml en string y luego a un objeto xml para buscar la informacion 
+    xml_data = ET.fromstring(responsedata)
+    data = []
+    cols = []
+    columnas = []
+    for child in xml_data:
+        for subchild in child:
+            for subsubchild in subchild:
+                for subsubsubchild in subsubchild:
+                    for subsubsubsubchild in subsubsubchild:
+                        data.append([subsubsubsubchild.text for subsubsubsubchild in subsubsubchild])
+                        cols.append(subsubsubsubchild.tag for subsubsubsubchild in subsubsubchild)
+                        
+                        #este if busca el nombre de las columnas
+                        if subsubsubsubchild.tag not in columnas:
+                            columnas.append(subsubsubsubchild.tag)
+    #se convierte la data en un dataframe de pandas y se rota 90°
+    df = pd.DataFrame(data).T
+    #se agrega una columna  con el nombre que van a tener las columnas una vez que se rote de nuevo 
+    df.columns = cols 
+    #se elimina la primer columna que era indice, se rota, y se eliminan duplicados de la columna codigo
+    df = df.iloc[:, 1:].T.set_axis(columnas, axis=1).drop_duplicates(subset=['codigo'])   
+
+    #se intenta eliminar las columnas innecesarias
+    try:
+        del df["cod_producto"]
+        del df["cod_proveedor"]     
+        del df["cod_hacienda"]  
+        del df["desc_corta"]
+        del df["modelo"]
+        del df["presentacion"]
+        del df["colores"]
+        del df["familia_id"]
+        del df["clase"]
+        del df["item_grupo_1"]
+        del df["item_grupo_2"]
+        del df["item_grupo_3"]
+        del df["image_url_2"]
+        del df["image_url_3"]
+        del df["image_url_4"]
+        del df["image_url_5"]
+        del df["caracteristicas"]
+        del df["itemref_1"]
+        del df["itemref_2"]
+        del df["itemref_3"]
+        del df["itemref_4"]
+        del df["itemref_5"]
+        del df["itemref_6"]
+        del df["itemref_7"]
+        del df["itemref_8"]
+        del df["itemref_9"]
+        del df["itemref_10"]
+        del df["itemref_11"]
+        del df["itemref_12"]
+        del df["itemref_13"]
+        del df["itemref_14"]
+        del df["privacidad"]
+        del df["peso"]
+        del df["medida"]
+        del df["equivalencia"]
+    
+    except KeyError as e:
+        print("ERROR: Una columna original se le cambio el nombre o ya no existe")
+
+    #se le pone el prefijo a las columnas para obtener una url con la imagen
+    prefijo = "eurocompcr.com/"
+    df['image_url'] = df['image_url'].apply(lambda x: prefijo + x if x else '')
+    df['image_url_1'] = df['image_url_1'].apply(lambda x: prefijo + x if x else '')
+
+    #se concatenan las columnas para crear una sola y se eliminan las anteriore
+    separador = ';'
+    df['productImageUrl'] = df['image_url'] + separador + df['image_url_1']
+    del df["image_url"]
+    del df["image_url_1"]
+
+    #reemplaza las filas que solo contienen ";"
+    df['productImageUrl'] = df['productImageUrl'].apply(lambda x: '' if x == ';' else x)
+
+    #renombra las columnas
+    df = df.rename(columns={"codigo":"sku"}, inplace=False)
+    df = df.rename(columns={"descripcion":"name"}, inplace=False)
+    df = df.rename(columns={"familia":"collection"}, inplace=False)      
+    df = df.rename(columns={"marca":"brand"}, inplace=False)
+    df = df.rename(columns={"precio":"price"}, inplace=False)
+    df = df.rename(columns={"stock":"inventory"}, inplace=False)
+
+    #hace el cambio de dolares a colones, multiplica por el IVA y un 20% de ganancia
+    df['price'] = df['price'].astype(float)
+    df['inventory'] = df['inventory'].astype(float)
+    df['price'] = round((df['price'] * 1.13 * 1.25 * (tipoDeCambio() + 10.0)) / 1000.0, 2) * 1000.0
+    
+    #elimina los productos que tienen 0 de precio 
+    df = df.loc[df['price'] != 0]
+    df = df.loc[df['inventory'] != 0]
+    df = df.loc[df['inventory'] != 1]
+    df = df.loc[df['inventory'] != 2]
+
+    #reemplaza los inventarios mayores a 30 por 30
+    df.loc[df['inventory'] > 30, 'inventory'] = 30
+
+    #crea las columnas nuevas 
+    df["handleId"] = "handleId_" + df["sku"] 
+    df["fieldType"] = "Product" 
+    df["description"] = """Imágenes de referencia
+    ¿Cómo comprar?
+    1-Agrega al carrito en “COMPRAR”
+    2-Registro: Completa tus datos y sigue los pasos
+    3-Selecciona el método de entrega: """
+    df["visible"] = "TRUE"
+    df["discountMode"] = "PERCENT"
+    df["discountValue"] = 0 
+    
+    listColumnasNuevasVacias = ["ri`bbon","surcharge","weight","cost","productOptionName1","productOptionType1","productOptionDescription1","productOptionName2","productOptionType2","productOptionDescription2","productOptionName3","productOptionType3","productOptionDescription3","productOptionName4","productOptionType4","productOptionDescription4","productOptionName5","productOptionType5","productOptionDescription5","productOptionName6","productOptionType6","productOptionDescription6","additionalInfoTitle1","additionalInfoDescription1","additionalInfoTitle2","additionalInfoDescription2","additionalInfoTitle3","additionalInfoDescription3","additionalInfoTitle4","additionalInfoDescription4","additionalInfoTitle5","additionalInfoDescription5","additionalInfoTitle6","additionalInfoDescription6","customTextField1","customTextCharLimit1","customTextMandatory1","customTextField2","customTextCharLimit2","customTextMandatory2"]
+
+    for columna in listColumnasNuevasVacias:
+        df[columna] = ""
+
+    df = df[['handleId','fieldType','name','description','productImageUrl','collection','sku','ribbon','price','surcharge','visible','discountMode','discountValue','inventory','weight','cost','productOptionName1','productOptionType1','productOptionDescription1','productOptionName2','productOptionType2','productOptionDescription2','productOptionName3','productOptionType3','productOptionDescription3','productOptionName4','productOptionType4','productOptionDescription4','productOptionName5','productOptionType5','productOptionDescription5','productOptionName6','productOptionType6','productOptionDescription6','additionalInfoTitle1','additionalInfoDescription1','additionalInfoTitle2','additionalInfoDescription2','additionalInfoTitle3','additionalInfoDescription3','additionalInfoTitle4','additionalInfoDescription4','additionalInfoTitle5','additionalInfoDescription5','additionalInfoTitle6','additionalInfoDescription6','customTextField1','customTextCharLimit1','customTextMandatory1','customTextField2','customTextCharLimit2','customTextMandatory2','brand']]
+
+    print("Guardando archivo de Excel...")
+    df.to_csv("Inventario_EUROCOMP.csv",index=False, encoding='utf-8-sig')
+    csv = df.to_csv(index=False, encoding='utf-8-sig')
+    print(csv)
+    print("Archivo de Excel guardado exitosamente")
+
+def main():
+    responsedata = realizarsolicitudsoap()
+    guardar(responsedata, "Inventario_EUROCOMP.xml")
+    #actualizarDatosExcel()   
+    transformar(responsedata)
+    #exportarExcelACsv()
+
+main()
